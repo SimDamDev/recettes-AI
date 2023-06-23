9e8472a19edab914c039e276993c0ffabbd0eb79
SimDamDev <contact@dev.simdam.ch>
2023-06-24 00:31:31 +0200

    bouton generer/ajouter, connection api gpt

--- public/addIngredient.js
+++ public/addIngredient.js
@@ -2,7 +2,65 @@
 const form = document.querySelector('form');
 const switchInput = document.getElementById('pluralSwitch');
 
-// Add an event listener for the form submission
+
+
+// Add an event listener for the focus event
+switchInput.addEventListener('focus', function() {
+    // Remove focus from the switch
+    this.blur();
+  });
+
+// Select the plural name field
+const pluralNameField = document.getElementById('pluralName');
+
+pluralNameField.disabled = true;
+
+
+switchInput.addEventListener('change', function() {
+    pluralNameField.disabled = !this.checked;
+});
+
+const autoFillSwitch = document.getElementById('autoFill');
+const pluralSwitch = document.getElementById('pluralSwitch');
+const unitsField = document.getElementById('units');
+
+// Add an event listener to the autoFill switch
+autoFillSwitch.addEventListener('change', function() {
+  // If the autoFill switch is checked, disable the pluralName field and the units field
+  if (this.checked) {
+    pluralNameField.disabled = true;
+    pluralSwitch.disabled = true;
+    unitsField.disabled = true;
+  } else {
+    // If the autoFill switch is not checked, enable the pluralName field and the units field
+    pluralNameField.disabled = false;
+    pluralSwitch.disabled = false;
+    unitsField.disabled = false;
+  }
+  // Update the button label whenever the switch is toggled
+  changeButtonLabel(this.checked);
+});
+
+document.getElementById('submitButton').addEventListener('click', async function() {
+  const name = document.getElementById('name').value;
+  const autoFill = document.getElementById('autoFill').checked;
+
+  if (autoFill) {
+    console.log("GPT-3 Request:", name); // affichez le nom de l'ingrédient dans la console
+      const response = await fetch('/api/gpt', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ name }) // envoyez juste le nom de l'ingrédient
+      });
+
+      const data = await response.json();
+      console.log("GPT-3 Response:", data); // affichez la réponse de GPT-3 dans la console
+
+      // Utilisez les données générées pour remplir les champs de formulaire ici
+  } else {
+     // Add an event listener for the form submission
 form.addEventListener('submit', event => {
   // Prevent the form from being submitted the default way
   event.preventDefault();
@@ -31,56 +89,13 @@ form.addEventListener('submit', event => {
     // Here you can handle errors, like showing an error message
   });
 });
-
-// Add an event listener for the focus event
-switchInput.addEventListener('focus', function() {
-    // Remove focus from the switch
-    this.blur();
-  });
-
-  // Select the plural name field
-const pluralNameField = document.getElementById('pluralName');
-
-pluralNameField.disabled = true;
-
-
-switchInput.addEventListener('change', function() {
-    pluralNameField.disabled = !this.checked;
-  });
-
-const autoFillSwitch = document.getElementById('autoFill');
-const pluralSwitch = document.getElementById('pluralSwitch');
-const unitsField = document.getElementById('units');
-
-  // Add an event listener to the autoFill switch
-  autoFillSwitch.addEventListener('change', function() {
-    // If the autoFill switch is checked, disable the pluralName field and the units field
-    if (this.checked) {
-      pluralNameField.disabled = true;
-      pluralSwitch.disabled = true;
-      unitsField.disabled = true;
-    } else {
-      // If the autoFill switch is not checked, enable the pluralName field and the units field
-      pluralNameField.disabled = false;
-      pluralSwitch.disabled = false;
-      unitsField.disabled = false;
-    }
-  });
-
-  document.getElementById('generateButton').addEventListener('click', async function() {
-    console.log("GPT-3 Request:", document.getElementById('name').value); // affichez le nom de l'ingrédient dans la console
-    const name = document.getElementById('name').value;
-
-    const response = await fetch('/api/gpt', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ name }) // envoyez juste le nom de l'ingrédient
-    });
-
-    const data = await response.json();
-    console.log("GPT-3 Response:", data); // affichez la réponse de GPT-3 dans la console
+  }
 });
 
-  //TODO quantité a gerer das recette et pas ici!
+function changeButtonLabel(isAutoFill) {
+    const submitButton = document.getElementById('submitButton');
+    submitButton.textContent = isAutoFill ? 'Générer' : 'Ajouter';
+}
+
+// Initialise le label du bouton
+changeButtonLabel(document.getElementById('autoFill').checked);
