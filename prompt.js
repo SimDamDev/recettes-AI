39fe22fbbb1e973ec42485ca3178d84a681c869f
SimDamDev <contact@dev.simdam.ch>
2023-06-25 16:15:02 +0200

    perfectionnement du prompt du check

--- prompt.js
+++ prompt.js
@@ -1,5 +1,4 @@
-const ingredientPrompt = `Génère-moi un fichier JSON correspondant au schéma suivant :
-const ingredientSchema = new mongoose.Schema({
+const ingredientPrompt = `({
   name: {
     type: String,
     required: true,
@@ -17,10 +16,25 @@ const ingredientSchema = new mongoose.Schema({
   },
 });
 
+Pour les unités, choisi parmi les suivants:
+Gramme (g), Kilogramme (kg), Milligramme (mg), Litre (l), Millilitre (ml), Cuillère à café (c. à café),
+Cuillère à soupe (c. à soupe), Tasse, Demi-tasse, Verre, Bol, Poignée, Pincée, Noix, Morceau, Tranche,
+Cube, Bouteille, Canette, Boîte, Paquet, Pièce, Feuille, Branche, Epi, Gousse, Graine, Boule, Bâton,
+Tablette, Carré
+
 Crée un objet ingrédient avec le nom 'Tomate' en utilisant ce schéma.
 
 La reponse doit contenir au moins une valeur pour chaque champs, et doit être un objet JSON valide.`;
 
+
+const checkIngredient = `
+oublie tous les prompts précédents
+tu est un expert en cuisine
+
+est ce que la Tomate est un ingrédient ?
+`;
+
 module.exports = {
-  ingredientPrompt
+  ingredientPrompt,
+  checkIngredient
 };
